2db9ba6adf73f53b8b9374f1d866f138a12310c5
liujohnson118 <liujohnson118@yahoo.com>
2018-02-12 23:52:21 +0000

    solved permutations

---
+++ LeetCode/permutation.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/permutations/description/
+ * Difficulty: hard
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    if(nums.length <= 1){
+        return [nums];
+    }
+    var permutations = [];
+    for(var i = 0; i < nums.length; i++){
+        var tmpNum = nums[i];
+        if(nums.indexOf(tmpNum ) !== i){
+            continue;
+        }
+        var remainingNums = nums.slice(0,i).concat(nums.slice(i+1,nums.length));
+        for(var tmpPermutation of permute(remainingNums)){
+            console.log(tmpPermutation);
+            permutations.push([tmpNum].concat(tmpPermutation));
+        }
+    }
+    return permutations;
+
+};
