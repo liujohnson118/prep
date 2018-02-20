7deeab6b0fefee9f2066b2473a7c08224083c6bd
liujohnson118 <liujohnson118@yahoo.com>
2018-02-20 00:59:06 +0000

    permutations ruby accepted

---
+++ LeetCode/permutation.rb
@@ -0,0 +1,25 @@
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+   if nums.length <= 1
+    return [nums]
+   end
+   i = 0
+   ret = []
+   for i in 0..nums.length
+    tmpNum = nums[i]
+    if nums.index(tmpNum).to_i<i
+      next
+    end
+    remainingNums = nums.slice(0,i).concat(nums.slice(i+1,nums.length))
+    permute(remainingNums).each{|tmpPermutation|
+      ret.push([tmpNum].concat(tmpPermutation))
+    }
+  end
+  return ret
+end
+
+myArr = permute([1,2,3])
+myArr.each{|group|
+  puts group
+}
